c132f1234de85aabc0b697c9dd81e1d60187c49b
Austin English <englisha@uci.edu>
2019-01-21 15:54:18 -0800

    working on articleController

--- back-end/Controllers/ArticleController.cs
+++ back-end/Controllers/ArticleController.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Xml;
+using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -17,6 +19,87 @@ namespace back_end.Controllers
             _context = context;
         }
 
+
+         void ParseRssFile(String xml)
+        {
+            XmlDocument rssXmlDoc = new XmlDocument();
+
+            // Load the RSS file from the RSS URL
+            rssXmlDoc.Load(xml);
+
+            // Parse the Items in the RSS file
+            XmlNodeList rssNodes = rssXmlDoc.SelectNodes("rss/channel/item");
+
+            StringBuilder rssContent = new StringBuilder();
+
+            // Iterate through the items in the RSS file
+            foreach (XmlNode rssNode in rssNodes)
+            {
+                XmlNode rssSubNode = rssNode.SelectSingleNode("title");
+                string title = rssSubNode != null ? rssSubNode.InnerText : "";
+
+                rssSubNode = rssNode.SelectSingleNode("link");
+                string link = rssSubNode != null ? rssSubNode.InnerText : "";
+
+                rssSubNode = rssNode.SelectSingleNode("description");
+                string description = rssSubNode != null ? rssSubNode.InnerText : "";
+
+                if (title.Contains("compliance")
+                || title.Contains("regulation")
+                || title.Contains("approves")
+                || description.Contains("regulation"))
+                {
+                    //if no matches with the database, we need to add it. 
+                    // aka: (new article published from websites below)
+                    if((_context.articles.FirstOrDefault(a => a.title == title))==null)
+                    {
+                        Article tempArticle = new Article();
+
+                    }
+                    
+                    rssContent.Append("<a href='" + link + Environment.NewLine);
+
+                }
+
+            }
+
+        }
+
+         void AddArticles()
+        {
+
+            List<String> links = new List<String>();
+            links.Add("https://mjbizdaily.com/feed/");
+            links.Add("https://cannabiz.media/feed/");
+            links.Add("https://www.liwts.org/feed/");
+            links.Add("https://www.covasoftware.com/blog/rss.xml");
+            links.Add("https://medicalmarijuana411.com/feed/");
+            links.Add("https://www.medicalmarijuanainc.com/feed/");
+            links.Add("https://grizzle.com/marijuana/feed/");
+            links.Add("https://merryjane.com/feed/all.rss");
+            links.Add("https://www.cannalawblog.com/feed/");
+            links.Add("https://news.weedmaps.com/feed/");
+            links.Add("https://hightimes.com/feed/");
+            links.Add("https://cannabislaw.report/feed/");
+            links.Add("http://www.cnbc.com/id/101432347/device/rss");
+            links.Add("https://cannabis.ca.gov/feed/");
+            links.Add("https://www.cannabisculture.com/feed/");
+            links.Add("https://mjobserver.com/feed/");
+            links.Add("https://www.leafly.com/feed");
+            links.Add("https://greencamp.com/feed/");
+            links.Add("https://thebluntinvestor.com/feed/");
+            links.Add("http://daggamagazine.com/feed/");
+            links.Add("https://www.crrh.org/news/taxonomy/term/3956/0/feed");
+            links.Add("http://marijuanaworldnews.com/feed/");
+            links.Add("https://www.marijuana.com/feed/");
+
+            foreach (var link in links)
+            {
+                ParseRssFile(link);
+            }
+
+        }
+
         // GET api
         [HttpGet]
         public ActionResult Get()
@@ -25,7 +108,10 @@ namespace back_end.Controllers
             {
                 return NoContent();
             }
-            return Ok(_context.articles.ToList());
+           
+                //need to add articles to DB
+                AddArticles();
+                return Ok(_context.articles.ToList());
         }
 
         // GET BY ID api
@@ -40,7 +126,7 @@ namespace back_end.Controllers
             return Ok(article);
         }
 
-        // POST api
+        // POST api may not need this, done when page is loaded
         [HttpPost]
         public ActionResult Post([FromBody] Article a)
         {
