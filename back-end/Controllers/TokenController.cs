7b20ba8faae1f40b4a61121d4f914f619dbe233e
Austin English <englisha@uci.edu>
2019-01-21 15:28:39 -0800

    fixing tokenController

--- back-end/Controllers/TokenController.cs
+++ back-end/Controllers/TokenController.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 using System.IdentityModel.Tokens.Jwt;
 using System.Text;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
-
 using BCrypt.Net;
 
 namespace back_end
@@ -19,20 +20,23 @@ namespace back_end
     public class TokenController : Controller
     {
         private IConfiguration _config;
-        public TokenController(IConfiguration config)
+        private DlylContext _context;
+        public TokenController(IConfiguration config,DlylContext context)
         {
-            _config = config;
+                _config = config;
+                _context = context;
         }
 
-        public string GetToken(string username, string password)
+        [HttpPost]
+         [Route("Register")]
+        public string Register([FromBody] User user)
         {
-            var user = _context.Users.FirstOrDefault(u => u.Username == username);
-            bool validPassword = BCrypt.Verify(password, hashedPassword);
-
-            if(username == user.username && validPassword)
-                return BuildToken();
-            else
-                return "";
+            
+            user.password = BCrypt.Net.BCrypt.HashPassword(user.password, SaltRevision.Revision2A);
+            _context.users.Add(user);
+            _context.SaveChanges();
+            return "created";
+           
         }
 
         private string BuildToken()
@@ -41,12 +45,12 @@ namespace back_end
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
             var token = new JwtSecurityToken(_config["Jwt:Issuer"],
-            _config["Jwt:Issuer"],
-            expires: DateTime.Now.AddMinutes(30),
-            signingCredentials: creds
-            );
+              _config["Jwt:Issuer"],
+              expires: DateTime.Now.AddMinutes(30),
+              signingCredentials: creds);
 
             return new JwtSecurityTokenHandler().WriteToken(token);
         }
+
     }
 }
