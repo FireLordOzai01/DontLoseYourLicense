f38774e9b64d3cd092c2da3715e02c2e49e350df
Heather Capati <heathercapati@Heathers-Air.attlocal.net>
2019-01-22 14:16:57 -0800

    created redux store and actions for users and articles

--- front-end/src/stores/store.js
+++ front-end/src/stores/store.js
@@ -1,10 +1,52 @@
+import {
+    ADD_USER, 
+    DELETE_USER,
+    EDIT_USER, 
+    GET_USERS,
+    ADD_ARTICLE,
+    DELETE_ARTICLE,
+    EDIT_ARTICLE,
+    GET_ARTICLES,
+
+} from '../constants';
+
 const initialState = {
-    users: []
+    users: [],
+    articles: []
 }
 
 const rootReducer = (state = initialState, action) => {
-
     switch(action.type){
+        case ADD_USER:
+        return {
+            ...state,
+            users: action.payload
+        }
+        case GET_USERS:
+        return {
+            ...state,
+            users: action.payload
+        }
+        case DELETE_USER:
+        return {
+            ...state,
+            users: action.payload
+        }
+        case ADD_ARTICLE:
+        return {
+            ...state,
+            articles: action.payload
+        }
+        case GET_ARTICLES:
+        return {
+            ...state,
+            articles: action.payload
+        }
+        case DELETE_ARTICLE:
+        return {
+            ...state,
+            articles: action.payload
+        }
         default:
         return state;
     }
