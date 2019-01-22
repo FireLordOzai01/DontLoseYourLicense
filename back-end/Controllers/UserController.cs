399dcb2514af406fd9651c69912e700846d22d85
ALEJANDRO OZAI <aozai@ALEJANDROs-MacBook-Pro.local>
2019-01-21 17:19:36 -0800

    hashed passwords and returning tokens

--- back-end/Controllers/UserController.cs
+++ back-end/Controllers/UserController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 
 namespace back_end.Controllers
 {
