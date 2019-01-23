c0f8c8cd7638db0f0fca18dd5b2df7bc4e1250bd
zbokhari <zbokhari@alumni.usc.edu>
2019-01-23 09:31:03 -0800

    home page updates regarding main nav and sidebar for articles and forum posts / commments.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,22 +1,36 @@
 import React, { Component } from 'react';
+
+//ROUTES
 import { Switch, Route } from 'react-router-dom';
+import Home from './components/home/home';
+
+
+//HOMEPAGE SECTIONS
+import LogoLogo from './components/logo/logo.js';
+import Navbar from './components/navbar/navbar.js';
 import './App.css';
 
-import Navbar from './components/navbar/navbar';
-import Home from './components/home/home';
-import Admin from './components/admin/admin';
-import MoreBooks from './components/books/moreBooks';
+// import Admin from './components/admin/admin';
+// import MoreBooks from './components/books/moreBooks';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Navbar/>
+      <div id="outer-wrapper" className="index home">
+        {/* Logo */}
+        <LogoLogo />
+        {/* Main Menu */}
+        <Navbar />
+        <div className="clear">
+        </div>
+        {/* Routes */}
         <Switch>
-          <Route exact path='/' render={(renderProps) => <Home /> } />
-          <Route  path='/more-books' render={(renderProps) => <MoreBooks /> } />
-          <Route  path='/admin' render={(renderProps) => <Admin /> } />
+          <Route exact path='/' render={(renderProps) => <Home />} />
+          {/* <Route  path='/admin' render={(renderProps) => <Admin /> } /> */}
         </Switch>
+        {/* End of Content Wrapper */}
+        <div className="clear">
+        </div>
       </div>
     );
   }
